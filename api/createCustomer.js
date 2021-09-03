570f0d30123c41496b5930f83cdbe02e8d37f9b1
edwardgower <edwardgower@gmail.com>
2021-09-03 11:23:18 -0700

    Initial commit of project

---
+++ api/createCustomer.js
@@ -0,0 +1,94 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+import createError from 'http-errors';
+import commonMiddleware from '../lib/commonMiddleware';
+import validator from '@middy/validator';
+import ajv from '../lib/schemas/ajvInstance';
+import createCustomerInputSchema from '../lib/schemas/createCustomerInputSchema';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function createCustomer(event, context) {
+  // create the response variables
+  let body;
+  let statusCode = 201;
+  const headers = { 'Content-Type': 'application/json' };
+
+  // parse the incoming body using middy
+  const { email, firstName, lastName } = event.body;
+  // create the internal consts
+  const customerId = uuid();
+  const timeNow = new Date();
+
+  // insert the customer of catch the errors
+  try {
+    // need to tranactionally write the email address to a seperate collection to ensure uniqueness
+    await dynamodb
+      .transactWrite({
+        TransactItems: [
+          {
+            Put: {
+              Item: {
+                // add the customer item to the collection
+                PK: 'C#' + customerId,
+                SK: 'C#' + customerId,
+                ID1: 'C#' + customerId,
+                Type: 'Customer',
+                ID5: email,
+                Name2: firstName,
+                Name4: lastName,
+                Created: timeNow.toISOString(),
+                Updated: timeNow.toISOString(),
+              },
+              TableName: process.env.CUSTOMERS_TABLE_NAME,
+              ConditionExpression: 'attribute_not_exists(PK)',
+            },
+          },
+          {
+            Put: {
+              // add the customers email to the collection to ensure uniqueness
+              Item: {
+                PK: 'C#' + email,
+                SK: 'C#' + email,
+                ID1: 'C#' + customerId,
+                ID5: email,
+                Created: timeNow.toISOString(),
+                Updated: timeNow.toISOString(),
+              },
+              TableName: process.env.CUSTOMERS_TABLE_NAME,
+              ConditionExpression: 'attribute_not_exists(PK)',
+            },
+          },
+        ],
+      })
+      .promise();
+  } catch (err) {
+    console.log(event);
+    console.log(err);
+    // todo: log errors specific to the reasons, i.e. let the client know if validation failed or transaction failed
+    throw new createError.InternalServerError(err);
+  } finally {
+    // build the response in the schema the client expects
+    body = JSON.stringify({
+      customerId,
+      email,
+      firstName,
+      lastName,
+    });
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+}
+
+export const handler = commonMiddleware(createCustomer).use(
+  validator({
+    inputSchema: createCustomerInputSchema,
+    ajvInstance: ajv,
+  })
+);
+
+//export const handler = createCustomer;
---
+++ api/getCustomer.js
@@ -0,0 +1,34 @@
+import createError from 'http-errors';
+import commonMiddleware from '../lib/commonMiddleware';
+import { getCustomerById } from '../lib/getCustomerById';
+
+async function getCustomer(event, context) {
+  // create the response variables
+  let body;
+  let statusCode = 200;
+  const headers = { 'Content-Type': 'application/json' };
+  let customer;
+
+  try {
+    const { id } = event.pathParameters;
+    customer = await getCustomerById(id);
+  } catch (error) {
+    throw new createError.InternalServerError(error);
+  } finally {
+    // build the response in the schema the client expects
+    body = JSON.stringify({
+      customerId: customer.ID1.replace('C#', ''),
+      email: customer.ID5,
+      firstName: customer.Name2,
+      lastName: customer.Name4,
+    });
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+}
+
+export const handler = commonMiddleware(getCustomer);
